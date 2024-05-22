20465b861700fc8e0e44cfa4c5b4475c27ed4f27
Joeries Young <joeries.young@gmail.com>
2024-05-22 09:26:38 +0800

    Make the generated dll file all in one.

--- src/CDMWrapper/CDMWrapper.cs
+++ src/CDMWrapper/CDMWrapper.cs
@@ -5,9 +5,9 @@ namespace CDMWrapper
 {
     public class CDMWrapper
     {
-        public void showCDM(long param)
+        public void showCDM(IntPtr param)
         {
-            IntPtr hwnd = (IntPtr)param;
+            IntPtr hwnd = param;
             System.Windows.Interop.HwndSourceParameters sourceParams = new System.Windows.Interop.HwndSourceParameters("CDMWrapper");
             sourceParams.ParentWindow = hwnd;
             sourceParams.WindowStyle = 0x10000000 | 0x40000000; // WS_VISIBLE | WS_CHILD; // style
